f8a55e3beffa4fc442e8e81e3ca9ade606b47130
Shahedul Hoque <217447shah@gmsil.com>
2025-08-27 12:40:45 +0600

    Show an alert with a message including the service name and number

--- script/index.js
+++ script/index.js
@@ -3,15 +3,22 @@
       return element;
      }
    
-    getElement('card-container').addEventListener('click',function(e){
+  const cardContainer =   getElement('card-container').addEventListener('click',function(e){
       if(e.target.className.includes('heart-btn')){
      const heart = getElement('total-heart').innerText
        const totalHeart = Number(heart)+1;
         getElement('total-heart').innerText = totalHeart;
       }
+       if(e.target.className.includes('call-button')){
+         const callButton = e.target
+         const serviceName = callButton.parentNode.parentNode.children[1].innerText
+         const serviceNumber = callButton.parentNode.parentNode.children[3].innerText
+         alert( '📞'+'calling'+' '+ serviceName +' ' +' '+ serviceNumber +'....')
+         
+      
 
-   
-     
+       }
+      
     })
 
       
